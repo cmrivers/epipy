06c5f525524c64b10d36445e27c61efe0c38a5f1
Caitlin Rivers <cmrivers@vbi.vt.edu>
2014-01-22 17:32:26 -0500

    fixed labeling. if label option is set to 'on', the first and last case in the cluster will be labeled with case's unique identifier

--- epipy/checkerboard.py
+++ epipy/checkerboard.py
@@ -15,13 +15,20 @@ from datetime import timedelta
 from itertools import cycle
 import numpy as np
 
-def checkerboard_plot(df, case_id, cluster_id, date_col):
+def checkerboard_plot(df, case_id, cluster_id, date_col, labels='on'):
     '''
-    df = pandas dataframe
+    PARAMETERS
+    ---------------------
+    df = pandas dataframe of line listing
     case_id = unique identifier of the cases
     cluster_id = identifier for each cluster, e.g. FamilyA
     date_col = column of onset or report dates
-    returns matplotlib figure and axis objects
+    labels = accepts 'on' or 'off'. Labels the first and last case in the cluster with
+            the unique case identifier.
+
+    RETURNS
+    ---------------------
+    matplotlib figure and axis objects
     '''
     clusters = epipy.group_clusters(df, cluster_id, date_col)
 
@@ -35,17 +42,21 @@ def checkerboard_plot(df, case_id, cluster_id, date_col):
     plt.ylim(1, len(grpnames))
     plt.yticks(np.arange(len(grpnames)), grpnames)
 
-    xtog = timedelta(((10*axprop[1]-axprop[0])/axprop[1]), 0, 0)
+    xtog = timedelta(((3*axprop[1]-axprop[0])/axprop[1]), 0, 0)
     counter = 0
     cols = cycle([color for i, color in enumerate(plt.rcParams['axes.color_cycle'])])
 
     for key, group in clusters:
         if len(group) > 1:
             color = next(cols)
-            casenums = cycle([int(num) for num in group.index])
+            casenums = [int(num) for num in group.index]
+            iter_casenums = cycle(casenums)
 
             positions = []
+
             for casedate in group[date_col].order():
+                curr_casenum = next(iter_casenums)
+
                 x1 = casedate
                 x2 = casedate + xtog
                 positions.append(x2)
@@ -56,16 +67,11 @@ def checkerboard_plot(df, case_id, cluster_id, date_col):
                 plt.fill_between([x1, x2], y1, y2, color=color, alpha=.3)
                 ypos = y1[0] + .5
 
-                try:
-                    if x1 <= positions[-2]:
-                        ypos = ypos + np.random.uniform(-.4, .4, 1)
-                except:
-                    pass
+                if curr_casenum == min(casenums) or curr_casenum == max(casenums):
+                    textspot = x1 + timedelta((x2 - x1).days/2.0, 0, 0)
+                    plt.text(textspot, ypos, curr_casenum, horizontalalignment='center',
+                    verticalalignment='center', fontsize=11, weight='bold')
 
-                textspot = x1 + timedelta((x2 - x1).days/2.0, 0, 0)
-                plt.text(textspot, ypos, next(casenums),
-			    horizontalalignment='center',
-			    verticalalignment='center', fontsize=9)
 
             counter += 1
 
