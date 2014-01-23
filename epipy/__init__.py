f5760fc8e32c6a34c6ca5cf2189c06f64dcdf400
Caitlin Rivers <caitlin.rivers@gmail.com>
2014-01-22 21:41:56 -0500

    added analyses.py

--- epipy/__init__.py
+++ epipy/__init__.py
@@ -2,6 +2,7 @@
 # tools
 from .data_generator import generate_example_data
 from .basics import date_convert, group_clusters, cluster_builder
+from .analyses import generation_analysis, reproduction_number
 
 # plotting
 from .case_tree import build_graph, case_tree_plot
