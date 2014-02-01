97bf1c83859ffd19dadde2ebb9892373e17e20a4
Caitlin Rivers <caitlin.rivers@gmail.com>
2014-01-31 20:18:37 -0500

    added 2x2 table functions to import

--- epipy/__init__.py
+++ epipy/__init__.py
@@ -2,7 +2,8 @@
 # tools
 from .data_generator import generate_example_data
 from .basics import date_convert, group_clusters, cluster_builder
-from .analyses import generation_analysis, reproduction_number
+from .analyses import generation_analysis, reproduction_number, create_2x2
+from .analyses import analyze_2x2, odds_ratio, relative_risk, chi2
 
 # plotting
 from .case_tree import build_graph, case_tree_plot
