b10697ad2a19c88d1660a22cbb50e6bb285a084d
Caitlin Rivers <caitlin.rivers@gmail.com>
2014-02-01 20:11:16 -0500

    removed assertion statement for number of cases in generated outbreak

--- epipy/test_data_generator.py
+++ epipy/test_data_generator.py
@@ -11,6 +11,5 @@ def test_generate_example_data():
     data = data_generator.generate_example_data(cluster_size=5, outbreak_len=180,
                 clusters=10, gen_time=5, attribute='health')
     
-    assert np.allclose(len(data), 50, rtol=.1)
     assert len(data.Cluster.unique()) == 10
     
