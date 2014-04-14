2b61a6d526c12d8a88b29f3e7675421492c4dac4
Caitlin <cmrivers@vbi.vt.edu>
2014-04-14 16:05:59 -0400

    outlining framework of flow

--- epipy/or_plot.py
+++ epipy/or_plot.py
@@ -1,15 +1,28 @@
 # usr/bin/python
 # -*- coding: utf-8 -*-
+
 '''
   -------------
  * Caitlin Rivers
  * [cmrivers@vbi.vt.edu](cmrivers@vbi.vt.edu)
   -------------
   '''
+
 import pandas as pd
-import matplotlib as mpl
-from matplotlib import pyplot
-import epipy as epi
+from matplotlib.pyplot import plt
+import analyses
+
+def _plot(ratios):
+    """
+    """
+
+    fig, ax = plt.subplots(figsize=(12, 8))
+    ax.set_aspect('auto')
+    ax.set_ylabel('Odds ratio')
+    ax.grid(True)
+
+    for ratio in ratios:
+
 
 
 def or_plot(df, risk_cols, outcome_col):
@@ -26,11 +39,20 @@ def or_plot(df, risk_cols, outcome_col):
     -color by OR
     -plot CI on scatterplot
     """
-    for col in risk_cols:
-        risk_order = ["{}".format(val) for val in risk_cols.values()]
-        outcome_order = ["{}".format(val) for val in outcome_col.values()]
-        table = epi.create_2x2(df, "{}".format(col), "{}".format(outcome_col), risk_order, outcome_order)
-        ratio, or_ci = epi.odds_ratio(table)
+
+    ratio_pairs = []
+    for col in df.itercols(): #this part isn't right...I forget how to select cols efficiently
+        if col.index in risk_cols:
+            risk_order = ["{}".format(val) for val in risk_cols.values()]
+            outcome_order = ["{}".format(val) for val in outcome_col.values()]
+            table = analyses.create_2x2(df, "{}".format(col), "{}".format(outcome_col), risk_order, outcome_order)
+            ratio, or_ci = epi.odds_ratio(table)
+            ratio_pairs.append({name:col.index, ratio:ratio, ci:or_ci})
+
+    _plot(ratio_pairs)
+
+
+
 
 
 
