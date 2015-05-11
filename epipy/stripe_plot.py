b2cd23460f489246e05b89bbc47cee532a684336
Caitlin <caitlin.rivers@gmail.com>
2015-05-11 18:35:13 -0400

    pointless debugging when really I just needed to restart the kernal

--- epipy/stripe_plot.py
+++ epipy/stripe_plot.py
@@ -6,11 +6,12 @@ Created on Sun Apr  5 09:02:11 2015
 """
 import pandas as pd
 import matplotlib.pyplot as plt
+import matplotlib.lines as mlines
 import numpy as np
 import seaborn as sns
 
 # %%
-def stripe_plot(df, yticks, date1, date2, color_col, color_dict=None, legend=True):
+def stripe_plot(df, yticks, date1, date2, color_col, color_dict=False, legend=True, fig=None, ax=None):
     """
     df = pandas dataframe of line list
     yticks = column with ytick labels, e.g. case number
@@ -23,27 +24,36 @@ def stripe_plot(df, yticks, date1, date2, color_col, color_dict=None, legend=Tru
     returns fig, ax
     ------------
     Example useage:
-    fig, ax = time_plot(cases, 'case_id', 'onset_date', 'combined_outcome_date', 'categorical_outcome')
+    fig, ax = stripe_plot(cases, 'case_id', 'onset_date', 'combined_outcome_date', 'categorical_outcome')
     """
-    fig, ax = plt.subplots()
+    if fig == None and ax == None:
+      fig, ax = plt.subplots()
+      ax.set_aspect('auto')
+      fig.autofmt_xdate()
+
     ax.xaxis_date()
-    ax.set_aspect('auto')
-    fig.autofmt_xdate()
+    ax.set_ylim(-1, len(df))
 
-    plt.ylim(-1, len(df))
-    plt.yticks(np.arange(len(df)), df[yticks].values)
 
-    color_keys = df[color_col].values
-    if color_dict == None:
-        color_choices = sns.color_palette('deep', len(color_keys))
-    color_dict = dict(zip(color_keys, color_choices))
+    if color_dict == False:
+        color_keys = df[color_col].unique()
+        color_tuple = sns.color_palette('deep', len(color_keys))
+        color_dict = dict(zip(color_keys, color_tuple))
+
+
+    if legend == True:
+        leg_items = []
+        for k, v in color_dict.iteritems():
+            item = mlines.Line2D([], [], color=v, marker='o', label=k)
+            leg_items.append(item)
+
+        ax.legend(handles=leg_items, loc='best')
+
 
     counter = 0
-
     for ix in df[date1].order(ascending=False).index:
         x1 = df.xs(ix)[date1]
         x2 = df.xs(ix)[date2]
-
         y1 = counter
 
         col = color_dict[df.xs(ix)[color_col]]
@@ -51,19 +61,10 @@ def stripe_plot(df, yticks, date1, date2, color_col, color_dict=None, legend=Tru
         ax.scatter(x1, y1, color=col)
         ax.scatter(x2, y1, color=col)
         plt.fill_between([x1, x2], y1-.04, y1+.04, alpha=.8, color=col)
-
         counter += 1
 
 
-    if legend == True:
-        lines = []
-
-        for key, value in color_dict.iteritems():
-            plt.scatter(df[date1][0], value[0], color=value, alpha=0)
-            line = plt.Line2D(range(1), range(1), color=value, marker='o', markersize=6, alpha=.8, label=key)
-            lines.append(line)
-
-        ax.legend(lines, [k for k in color_dict.iterkeys()])
+    plt.yticks(np.arange(len(df)), df[yticks].values)
 
     return fig, ax
 
