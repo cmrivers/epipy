162d4d73b4b0e51cece766efb8b30d0bb10bd447
Caitlin Rivers <cmrivers@vbi.vt.edu>
2016-02-02 15:26:11 -0500

    fix month tallies

--- epipy/epicurve.py
+++ epipy/epicurve.py
@@ -7,7 +7,7 @@ import pandas as pd
 import matplotlib.pyplot as plt
 
 
-def epicurve_plot(df, date_col, freq, title=None, fig= None, ax=None, color=None):
+def epicurve_plot(df, date_col, freq, fig= None, ax=None, color=None):
     '''
     Creates an epicurve (count of new cases over time)
 
@@ -29,37 +29,28 @@ def epicurve_plot(df, date_col, freq, title=None, fig= None, ax=None, color=None
 
     df = df[df[date_col].isnull() == False]
     freq = freq.lower()[0]
-    df.new_col = df[date_col]
-
+    ix = pd.date_range(df[date_col].min(), df[date_col].max(), freq=freq)
     
-
-    #count the number of cases per time period
     if freq == 'd':
         curve = pd.DataFrame(df[date_col].value_counts().sort_index())
-        ix = pd.date_range(df[date_col].min(), df[date_col].max(), freq=freq)
-        curve = curve.reindex(ix)
-        curve = curve.fillna(0)
-
 
     elif freq == 'm':
-	#convert dates to months
-        format_date = df.new_col.dropna().map(lambda x: str(x.strftime("%Y/%m")))
-        form = format_date.map(lambda x: epipy.basics.date_convert(x, "%Y/%m"))
-	#count number of cases per month
-        curve = pd.DataFrame(form.value_counts(), columns=['count'])
+        curve = pd.DataFrame(df[date_col].value_counts().sort_index())
+        curve = curve.resample(freq, how='sum')
 
     elif freq == 'y':
-	#convert dates to year
         df.new_col = df.new_col.dropna().map(lambda x: x.year)
-	#count number of cases per year
-        curve = pd.DataFrame(df.new_col.value_counts(), columns=['count'])
+        curve = pd.DataFrame(df.new_col.value_counts())
+    
+    curve = curve.reindex(ix).fillna(0)
+    curve = curve.rename(columns={date_col:'counts'})
 
-    fig, ax = _plot(curve, freq, fig, ax, title, color)
+    fig, ax = _plot(curve, freq, fig, ax, color)
 
     return curve, fig, ax
 
 
-def _plot(freq_table, freq, fig, ax, title, color):
+def _plot(freq_table, freq, fig, ax, color):
     '''
     Plot number of new cases over time
     freq_table = frequency table of cases by date, from epicurve()
@@ -67,33 +58,28 @@ def _plot(freq_table, freq, fig, ax, title, color):
     '''
 
     axprop =  ax.axis()
-    freq_table['plotdates'] = freq_table.index
 
     # care about date formatting
     if freq == 'd':
-        wid = ((2*axprop[1]-axprop[0])/axprop[1])
+        #wid = ((2*axprop[1]-axprop[0])/axprop[1])
         ax.xaxis_date()
         fig.autofmt_xdate()
 
     elif freq == 'm':
         ax.xaxis_date()
         fig.autofmt_xdate()
-        wid = len(freq_table)
+        #wid = len(freq_table)
 
     elif freq == 'y':
-        locs = freq_table['plotdates'].values.tolist()
+        locs = freq_table.index.tolist()
         labels = [str(loc) for loc in locs]
-        wid = 1
+        #wid = 1
         ax.set_xticks(locs)
         ax.set_xticklabels(labels)
 
     if color == None:
         color == 'b'
         
-    ax.bar(freq_table['plotdates'].values, freq_table['count'].values,
-	width=wid, align='center', color=color)
-
-    if title != None:
-        ax.set_title(title)
+    ax.bar(freq_table.index, freq_table['counts'].values, align='center', color=color)
 
     return fig, ax
