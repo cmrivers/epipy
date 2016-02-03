11d9da6d4a21e7052630b0d9831a0c4c2e9ef082
Caitlin Rivers <cmrivers@vbi.vt.edu>
2016-02-03 08:25:28 -0500

    updates

--- epipy/epicurve.py
+++ epipy/epicurve.py
@@ -1,7 +1,6 @@
 #! /usr/bin/env python
 # -*- coding: utf-8 -*-
 
-from __future__ import division
 import epipy
 import pandas as pd
 import matplotlib.pyplot as plt
@@ -13,9 +12,8 @@ def epicurve_plot(df, date_col, freq, fig= None, ax=None, color=None):
 
     df = pandas dataframe
     date_col = date used to denote case onset or report date
-    freq = desired plotting frequency. Can be day, month or year
-    title = optional
-    fig, ax = fig ax objects
+    freq = desired plotting frequency. Can be day (d), month (m) or year (y)
+    fig, ax = fig & ax objects
     color = value of bar color to be passed to matplotlib
     
     RETURNS
@@ -29,25 +27,20 @@ def epicurve_plot(df, date_col, freq, fig= None, ax=None, color=None):
 
     df = df[df[date_col].isnull() == False]
     freq = freq.lower()[0]
+
     ix = pd.date_range(df[date_col].min(), df[date_col].max(), freq=freq)
-    
-    if freq == 'd':
-        curve = pd.DataFrame(df[date_col].value_counts().sort_index())
 
-    elif freq == 'm':
-        curve = pd.DataFrame(df[date_col].value_counts().sort_index())
-        curve = curve.resample(freq, how='sum')
-
-    elif freq == 'y':
-        df.new_col = df.new_col.dropna().map(lambda x: x.year)
-        curve = pd.DataFrame(df.new_col.value_counts())
-    
-    curve = curve.reindex(ix).fillna(0)
+    curve = pd.DataFrame(df[date_col].value_counts().sort_index())
+    curve = curve
+    .resample(freq, how='sum', closed='right').reindex(ix).fillna(0)
     curve = curve.rename(columns={date_col:'counts'})
 
-    fig, ax = _plot(curve, freq, fig, ax, color)
-
-    return curve, fig, ax
+    try:
+        fig, ax = _plot(curve, freq, fig, ax, color)
+        return curve, fig, ax
+    except Exception as e:
+        print e
+        return curve, NaN, NaN
 
 
 def _plot(freq_table, freq, fig, ax, color):
@@ -58,28 +51,28 @@ def _plot(freq_table, freq, fig, ax, color):
     '''
 
     axprop =  ax.axis()
+    freq_table['plotdates'] = freq_table.index
 
     # care about date formatting
     if freq == 'd':
-        #wid = ((2*axprop[1]-axprop[0])/axprop[1])
+        wid = ((2*axprop[1]-axprop[0])/axprop[1])
         ax.xaxis_date()
         fig.autofmt_xdate()
 
     elif freq == 'm':
         ax.xaxis_date()
         fig.autofmt_xdate()
-        #wid = len(freq_table)
+        wid = len(freq_table)
 
     elif freq == 'y':
-        locs = freq_table.index.tolist()
+        locs = freq_table['plotdates'].values.tolist()
         labels = [str(loc) for loc in locs]
-        #wid = 1
+        wid = 1
         ax.set_xticks(locs)
         ax.set_xticklabels(labels)
 
     if color == None:
         color == 'b'
         
-    ax.bar(freq_table.index, freq_table['counts'].values, align='center', color=color)
-
-    return fig, ax
+    ax.bar(freq_table['plotdates'].values, freq_table['counts'].values,
+	width=wid, align='center', color=color)
